4a5f9c5699dd88922567c30a132123b088d37932
Evine Storm <evinestorm@gmail.com>
2019-07-27 13:05:52 +0200

    Added flash messages for creating, editing and deleting articles. Changed the root to display the index page.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,13 +16,17 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(params[:article])
         @article.save
-        
+
+        flash.notice = "Article '#{@article.title}' Created!"
+
         redirect_to article_path(@article)
     end
 
     def destroy
         @article = Article.find(params[:id])
         @article.destroy
+
+        flash.notice = "Article '#{@article.title}' Deleted!"
 
         redirect_to action: :index
     end
@@ -35,6 +39,8 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
         @article.update(article_params)
       
+        flash.notice = "Article '#{@article.title}' Updated!"
+
         redirect_to article_path(@article)      
     end
 
