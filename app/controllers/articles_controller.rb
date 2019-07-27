efa34c176ce5b9e9a2957aaab3783f6d912b2333
Evine Storm <evinestorm@gmail.com>
2019-07-27 12:54:47 +0200

    Added 'Edit article' function. Created a partial for the form needed for creating and editing an article.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,13 +16,27 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(params[:article])
         @article.save
+        
         redirect_to article_path(@article)
     end
 
     def destroy
         @article = Article.find(params[:id])
         @article.destroy
+
         redirect_to action: :index
     end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+      
+        redirect_to article_path(@article)      
+    end
+
 end
 
